60441c8ac9ed9b81533296a10fc69c3a3f44b968
OmarMadeIt <106669673+OmarMadeIt@users.noreply.github.com>
2022-06-06 23:11:04 +0000

    Add files via upload

--- Gestion_De_Queue.py
+++ Gestion_De_Queue.py
@@ -2,7 +2,6 @@ import streamlit as st
 from streamlit import caching
 import pandas as pd
 from datetime import datetime, date
-from streamlit_server_state import server_state, server_state_lock
 
 def cache_clear_dt(dummy):
    clear_dt = date.today()
@@ -10,21 +9,14 @@ def cache_clear_dt(dummy):
 if cache_clear_dt("dummy")<date.today():
    caching.clear_cache()
 
-with server_state_lock["n_init"]:  # Lock the "count" state for thread-safety
-    if "n_init" not in server_state:
-        server_state.n_init = 0
-#st.session_state.n_init=0    #A ne pas réctiver
-#if 'n_init' not in st.session_state:
-	#st.session_state.n_init = 0
+#st.session_state.n_init=0    
+if 'n_init' not in st.session_state:
+	st.session_state.n_init = 0
 
-#if st.session_state.n_init ==7:
-	#st.session_state.n_init = 0
-
-with server_state_lock["liste_num"]:  # Lock the "count" state for thread-safety
-    if "liste_num" not in server_state:
-        server_state.liste_num = []
-#if 'liste_num' not in st.session_state:
-    #st.session_state.liste_num = []
+if st.session_state.n_init ==7:
+	st.session_state.n_init = 0 
+if 'liste_num' not in st.session_state:
+    st.session_state.liste_num = []
 now=datetime.now()
 #n_init = 0
 #liste_num=[]
@@ -42,20 +34,22 @@ def main():
     if submit_button:
         st.info("Résultats")
         #num_tel = input("Merci d'indiquer votre numéro de téléphone ? : ")
-        if num_tel in server_state.liste_num:
+        if num_tel in st.session_state.liste_num:
             st.write("Vous ne pouvez pas vous inscrire, vous êtes déjà sur la liste")
         else:
-            st.write('ICI')
-            server_state.liste_num.append(str(num_tel))
+            
+            st.session_state.liste_num.append(str(num_tel))
             now=datetime.now()
-            server_state.n_init=server_state.n_init+1
+            st.session_state.n_init=st.session_state.n_init+1
             #dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
             dt_jour=now.strftime("%d/%m/%Y")
             dt_heure = now.strftime("%H:%M:%S")
             st.write("Bonjour "+Prenom)
             st.write("Nous sommes le ",dt_jour)
             st.write("Il est : ", dt_heure)
-            st.write("Vous êtes le numéro "+str(server_state.n_init))
+            st.write("Vous êtes le numéro "+str(st.session_state.n_init))
+            #results = Prenom+Nom+'@gmail.com'
+            #st.write(results)
             
 if __name__=='__main__':
     main()
